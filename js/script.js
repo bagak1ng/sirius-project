0e8ba5e920bafed2b1a5cfb15d2779e18e1f849c
bagak1ng <malinasamat@gmail.com>
2023-03-02 14:32:00 +0600

    fixed up click commit

--- js/script.js
+++ js/script.js
@@ -1,32 +1,64 @@
 var swiper = new Swiper(".cases-container", {
-    slidesPerView: 4,
-    spaceBetween: 40,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    gragCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 4,
+  spaceBetween: 40,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  gragCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    breakpoints: {
-      0: {
-        slidesPerView: 1
-      },
-      585: {
-        slidesPerView: 2,
-      },
-      995: {
-        slidesPerView: 3
-      },
-      1200: {
-        slidesPerView: 4
-      }
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    585: {
+      slidesPerView: 2,
+    },
+    995: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.remove('btn-up_hide');
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.add('btn-up_hide');
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener('scroll', () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.btn-up').onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
     }
-  });
+  }
+}
+
+btnUp.addEventListener();
